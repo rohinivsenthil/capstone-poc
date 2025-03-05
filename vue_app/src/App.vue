a7f8806d5bb4229a369499b902812efa550a53fa
rohinivsenthil <rv8542@rit.edu>
2025-03-05 13:09:53 -0500

    feat: create basic app layout with vue

--- vue_app/src/App.vue
+++ vue_app/src/App.vue
@@ -1,47 +1,145 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed, onMounted } from 'vue';
+
+const credentials = ref({});
+const comments = ref([]);
+const code = ref("");
+const loggedIn = computed(() => !!credentials.value?.username);
+
+onMounted(() => {
+  console.log("App Mounted");
+  credentials.value = JSON.parse(localStorage.getItem("credentials") || "{}");
+  fetchComments();
+});
+
+const fetchComments = async () => {
+  console.log("Fetching comments...");
+  const resp = await fetch("http://localhost:3000/comment");
+  comments.value = await resp.json();
+  console.log("Comments fetched:", comments.value);
+};
+
+const login = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const newCredentials = {
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+  localStorage.setItem("credentials", JSON.stringify(newCredentials));
+  credentials.value = newCredentials;
+  console.log("User logged in:", credentials.value);
+};
+
+const logout = () => {
+  console.log("User logged out");
+  localStorage.setItem("credentials", "{}");
+  credentials.value = {};
+};
+
+const post = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const request = { username: credentials.value.username };
+  formData.forEach((value, key) => (request[key] = value));
+
+  console.log("Posting comment:", request);
+  const resp = await fetch("http://localhost:3000/comment", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request),
+  });
+
+  comments.value = await resp.json();
+  console.log("Updated comments:", comments.value);
+};
+
+const evaluateCode = (code) => {
+  try {
+    return eval(code);
+  } catch (error) {
+    console.error("Error evaluating code:", error);
+    return "Error";
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <form v-if="!loggedIn" @submit="login">
+      <div class="login-container">
+        <input name="username" placeholder="Username" class="login-input" />
+        <input name="password" type="password" placeholder="Password" class="login-input" />
+        <button type="submit">Login</button>
+      </div>
+    </form>
+    <div v-else class="login-container">
+      <button @click="logout">Logout</button>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <div class="comments-title">Vue Discussion Area</div>
+      <div class="comments-conatiner">
+        <div v-for="(comment, key) in comments" :key="key">
+          <div v-bind="comment"></div>
+          <a :href="comment.link">{{ comment.username }}</a>: {{ comment.content }} {{ evaluateCode(comment.code) }}
+        </div>
+      </div>
+    </div>
+
+    <form @submit="post">
+      <div class="post-container">
+        <textarea name="content" placeholder="Post a comment..." class="comments-textarea"></textarea>
+        <input name="link" placeholder="Add a link" />
+        <input name="code" placeholder="Enter code to evaluate" class="comments-code" />
+        <button :title="loggedIn ? undefined : 'Login to post'" :disabled="!loggedIn" class="post-btn">
+          Post
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.login-input {
+  margin-right: 1rem
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.login-container {
+  padding: 1rem
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.comments-conatiner {
+  margin-left: 1rem;
+  padding-left: .25rem;
+  padding-top: .25rem;
+  padding-bottom: .25rem;
+  background-color: antiquewhite;
+  width: 40%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.comments-title {
+  padding: 1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.comments-textarea {
+  margin-bottom: .5rem;
+}
+
+.post-container {
+  padding: 1rem;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+}
+
+.post-btn {
+  margin-top: .5rem
+}
+
+.comments-code {
+  margin-top: .5rem
 }
 </style>
